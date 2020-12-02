b7d72fc1cddca3c2d664afcfaf7de469325ffb35
manucher <manucher.tursunov@gmail.com>
2020-12-02 23:15:23 +0500

    Passenger almost done

--- Passenger.java
+++ Passenger.java
@@ -1,25 +1,29 @@
+/*осталось проверить есть ли у пассажира открытый request и во время подсчета проверить дистанцию таксистов*/
 import java.io.*;
 import java.sql.*;
 import java.util.Scanner;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+
 public class Passenger {
 
     final Connection conn;
     final Scanner in;
     int input;
+
     public Passenger(Connection conn, Scanner in) {
         this.conn = conn;
         this.in = in;
     }
+
     public void start() {
         System.out.println("Passenger, what would you like to do?");
         System.out.println("1. Request a ride");
         System.out.println("2. Check trip records");
         System.out.println("3. Go back");
 
-        while(true) {
+        while (true) {
             System.out.println("Please enter[1-3]");
             input = in.nextInt();
             if (1 <= input && input <= 3) {
@@ -28,85 +32,115 @@ public class Passenger {
                 System.out.println("[ERROR] Invalid input.");
             }
         }
-        if (input == 1)
-        {
+        if (input == 1) {
             make_request();
-        }
-        else if (input == 2)
-        {
+        } else if (input == 2) {
             check_trip();
         }
 
     }
-    public void make_request()
-    {
+
+    public void make_request() {
         int p_id, pnum, d_years;
         String start_loc, end_loc, model;
         String dd_years;
         System.out.println("Please enter your ID.");
         p_id = in.nextInt();
         System.out.println("Please enter the number of passengers.");
-        pnum = in.nextInt();
-        in.nextLine();
-        System.out.println("Please enter the start location.");
-        start_loc = in.nextLine();
-        System.out.println("Please enter the destination.");
         while (true)
         {
+            pnum = in.nextInt();
+            if (pnum>8)
+                System.out.println("[Error] Invalid number of passengers");
+            else 
+                break;
+        }
+        in.nextLine();
+        while (true) {
+            System.out.println("Please enter the start location.");
+            start_loc = in.nextLine();
+            PreparedStatement stmtc;
+            try {
+                stmtc = conn.prepareStatement("select count(case when name like \'" + start_loc
+                        + "\' then 1 else null end) as num from taxi_stops");
+                ResultSet res = stmtc.executeQuery();
+                res.next();
+                int count = res.getInt(1);
+                if (count == 0) {
+                    System.out.println("[ERROR] Start Location not found");
+                } else
+                    break;
+            } catch (SQLException e) {
+                System.out.println("[ERROR] " + e);
+                System.exit(1);
+            }
+        }
+        while (true) {
+            System.out.println("Please enter the destination.");
             end_loc = in.nextLine();
-            if (end_loc == start_loc)
-            System.out.println("[ERROR] destination and start location should be different.");
-            else
-            break;
+            PreparedStatement stmtc;
+            try {
+                stmtc = conn.prepareStatement("select count(case when name like \'" + end_loc
+                        + "\' then 1 else null end) as num from taxi_stops");
+                ResultSet res = stmtc.executeQuery();
+                res.next();
+                int count = res.getInt(1);
+                if (count == 0) {
+                    System.out.println("[ERROR] Destination not found");
+                } else if (end_loc.equals(start_loc))
+                    System.out.println("[ERROR] Destination and start location should be different.");
+                else
+                    break;
+            } catch (SQLException e) {
+                System.out.println("[ERROR] " + e);
+                System.exit(1);
+            }
         }
         System.out.println("Please enter the model. (Press enter to skip)");
         model = in.nextLine();
         System.out.println("Please enter the minimum driving years of the drivers. (Press enter to skip)");
         dd_years = in.nextLine();
-        if (dd_years.length()==0)
+        if (dd_years.length() == 0)
             d_years = 0;
         else
             d_years = Integer.parseInt(dd_years);
-        PreparedStatement stmt,stmt2;
-        try
-        {
-            if (model.length()!=0) {
+        PreparedStatement stmt, stmt2;
+        try {
+            if (model.length() != 0) {
                 stmt2 = conn.prepareStatement(
-                        "insert into requests (taken,model,driving_years,passenger_id,passengers,start_location,finish_location) values (0,\'" +  model + "\'," + d_years + "," + p_id + "," + pnum + ",\'" + start_loc + "\',\'" + end_loc + "\')"); 
+                        "insert into requests (taken,model,driving_years,passenger_id,passengers,start_location,finish_location) values (0,\'"
+                                + model + "\'," + d_years + "," + p_id + "," + pnum + ",\'" + start_loc + "\',\'"
+                                + end_loc + "\')");
             } else {
                 stmt2 = conn.prepareStatement(
-                    "insert into requests (taken,model,driving_years,passenger_id,passengers,start_location,finish_location) values (0,null," + d_years + "," + p_id + "," + pnum + ",\'" + start_loc + "\',\'" + end_loc + "\')");
+                        "insert into requests (taken,model,driving_years,passenger_id,passengers,start_location,finish_location) values (0,null,"
+                                + d_years + "," + p_id + "," + pnum + ",\'" + start_loc + "\',\'" + end_loc + "\')");
             }
             stmt2.execute();
-        }
-        catch (SQLException e) 
-        {
+        } catch (SQLException e) {
             System.out.println("[ERROR] " + e);
             System.exit(1);
         }
-       // System.out.println("second one ");
-        try 
-        {
-            if (model.length()!=0) {
-                stmt = conn.prepareStatement(
-                        "select count(case when vehicles.seats >="
-                                + pnum + " and driving_years>=" + d_years + " and vehicles.model like \'%" + model + "%\' then 1 else null end) as num from drivers cross join vehicles on vehicle_id=vehicles.id");
+        try {
+            if (model.length() != 0) {
+                stmt = conn.prepareStatement("select count(case when vehicles.seats >=" + pnum + " and driving_years>="
+                        + d_years + " and vehicles.model like \'%" + model
+                        + "%\' then 1 else null end) as num from drivers cross join vehicles on vehicle_id=vehicles.id");
             } else {
-                stmt = conn.prepareStatement(
-                        "select count(case when vehicles.seats >="
-                                + pnum + " and driving_years>=" + d_years + " then 1 else null end) as num from drivers cross join vehicles on vehicle_id=vehicles.id");
+                stmt = conn.prepareStatement("select count(case when vehicles.seats >=" + pnum + " and driving_years>="
+                        + d_years
+                        + " then 1 else null end) as num from drivers cross join vehicles on vehicle_id=vehicles.id");
             }
             ResultSet res = stmt.executeQuery();
             res.next();
             int count = res.getInt(1);
-            System.out.println("Your request is placed. " + count + "drivers are able to take request.");
-        } 
-        catch (SQLException e) 
-        {
+            System.out.println("Your request is placed. " + count + " drivers are able to take request.");
+        } catch (SQLException e) {
             System.out.println("[ERROR] " + e);
             System.exit(1);
         }
     }
+
     public void check_trip()
     {
         int id;
@@ -119,36 +153,12 @@ public class Passenger {
         date = in.nextLine();
         date += " 00:00:00";
         start_time=date;
-        /*try
-        {
-            x=dateFormat.parse(date);
-        }
-        catch (ParseException e)
-        {
-            e.printStackTrace();
-        }
-        //System.out.println(x);
-        start_time = new Timestamp(x.getTime());
-        */
         System.out.println("Please enter the end date.");
         date = in.nextLine();
         date += " 23:59:59";
         finish_time=date;
-        /*try
-        {
-           x=dateFormat.parse(date);
-        }
-        catch (ParseException e)
-        {
-            e.printStackTrace();
-        }
-        //System.out.println("-------------"+date);
-        //System.out.println(x);
-        finish_time=new Timestamp(x.getTime());
-        */
         System.out.println("Please enter the destination.");
         destination = in.nextLine();
-        System.out.println(destination);
         PreparedStatement stmt;
         try 
         {
@@ -156,7 +166,8 @@ public class Passenger {
                 st1="select T.id,D.name,V.id,V.model,start_time,finish_time,fee,start_location,finish_location ";
                 st2="from trips as T cross join drivers as D on driver_id = D.id cross join vehicles as V on D.vehicle_id like V.id ";
                 stmt = conn.prepareStatement(
-                        st1+st2+"where T.passenger_id=" + id + " and timestampdiff(DAY,\'"+start_time+"\',T.start_time)>0 and timestampdiff(DAY,T.finish_time,\'"+finish_time+"\')>0 and T.finish_location like \'" + destination + "\'");                
+                        st1+st2+"where T.passenger_id=" + id + " and timestampdiff(DAY,\'"+start_time+"\',T.start_time)>0 and timestampdiff(DAY,T.finish_time,\'"+finish_time
+                        + "\')>0 and T.finish_location like \'" + destination + "\' order by start_time");                
                 ResultSet res = stmt.executeQuery();
                 ResultSetMetaData rsmd = res.getMetaData();
                 int columnsNumber = rsmd.getColumnCount();
@@ -179,10 +190,5 @@ public class Passenger {
     }
 }
 /*
-2
-2
-1
-2018-01-01
-2018-12-31
-Sham Shui Po
-*/
+ * 2 2 1 2018-01-01 2018-12-31 Sham Shui Po
+ */
